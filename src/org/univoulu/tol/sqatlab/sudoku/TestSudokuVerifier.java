54dfeeacb72ee136cb32c8b789c9dbeca6b993a8
somename <someemail>
2015-09-30 17:35:08 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
-
+	
+	stop hier
+	
+	
 	SudokuVerifier sudokuVerifier;
 	String correctSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";	
 	String wrongSudoku = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
