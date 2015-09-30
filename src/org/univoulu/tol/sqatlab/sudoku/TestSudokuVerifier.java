f641880df53018df0e03b942e49f44cebd1e811c
somename <someemail>
2015-09-30 14:54:02 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -6,9 +6,20 @@ import org.junit.Test;
 
 public class TestSudokuVerifier {
 
+	String correctSudoku ="417369825632158947958724316825437169791586432346912758289643571573291684164875293";	
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	@Test
+	public void testChangeStringToIntArray() {
+		int[] numbers = changeStringToIntArray(correctSudoku);
+		
+		assertEquals(numbers[0], 4);
+		assertEquals(numbers[1], 1);
+		assertEquals(numbers[2], 7);
+		assertEquals(numbers[80], 3);
 	}
 
 }
