855c6303caf16c5d4699a7cf443d4d61dd1c4dd6
somename <someemail>
2015-09-30 15:50:36 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -28,14 +28,14 @@ public class TestSudokuVerifier {
 		assertEquals(0, validity);
 	}
 	
-	/*
+	
 	@Test
 	public void testVerify_WrongString_ReturnsMinusOne(){
 		sudokuVerifier = new SudokuVerifier();
 		int validity = sudokuVerifier.verify("a922222222222");
 		assertEquals(-1, validity);
 	}
-	*/
+	
 	
 	@Test
 	public void testCheckRuleOne_CorrectStringReturnTrue(){
