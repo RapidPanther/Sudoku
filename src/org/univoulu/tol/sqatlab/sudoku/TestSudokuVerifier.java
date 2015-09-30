69e25a7ff7f76b7e8fbf9b31a69bb51c171bedd1
somename <someemail>
2015-09-30 17:09:44 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -11,6 +11,7 @@ public class TestSudokuVerifier {
 	String wrongSudoku = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	String failRuleOne = "-417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	String failRuleTwo = "447369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	String failRuleThree = "447369825632158947958724316825437169791586432346912758289643571573291684164875293";	
 	
 	@Test
 	public void testChangeStringToIntArray() {
@@ -70,7 +71,7 @@ public class TestSudokuVerifier {
 	@Test
 	public void testCheckRuleThree_IncorrectSolutionReturnFalse(){
 		sudokuVerifier = new SudokuVerifier();
-		assertEquals(true, sudokuVerifier.checkRuleThree(failRuleThree));
+		assertEquals(false, sudokuVerifier.checkRuleThree(failRuleThree));
 	}
 	
 	
