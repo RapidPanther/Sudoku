121bab46c39dd0a75b2af93164c434e3241005b9
somename <someemail>
2015-09-30 16:57:13 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -86,7 +86,8 @@ public class SudokuVerifier {
 		for(int i = 0; i < 3; i++)
 		{
 			for(int j = 0; j < 3; j++){
-				digit[counter] = numbers[i][j];  
+				digit[counter] = numbers[i][j];
+				System.out.print(digit[counter]);
 			}
 		}
 		
