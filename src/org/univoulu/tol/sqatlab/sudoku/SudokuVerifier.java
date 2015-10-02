dce45c8bb25e7de06eee7736f08d91a1cde09013
somename <someemail>
2015-10-02 21:53:52 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -100,10 +100,7 @@ public class SudokuVerifier {
 		int yValue = 0;
 		
 		while(digitCounterX <= 2 && digitCounterY <= 2)
-		{
-			xValue = 0;
-			yValue = 0;
-			
+		{			
 			for(int i = 0; i < 3; i++)
 			{
 				for(int j = 0; j < 3; j++)
